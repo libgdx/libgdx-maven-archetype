32c933c1f0ea79a040c235312e55592a67d48530
Mario Zechner <contact@badlogicgames.com>
2012-10-21 18:47:59 +0200

    initial clusterfuck

---
+++ src/main/resources/archetype-resources/ios/Main.cs
@@ -0,0 +1,27 @@
+using System;
+using MonoTouch.Foundation;
+using MonoTouch.UIKit;
+
+using playn.ios;
+using playn.core;
+using ${package}.core;
+
+namespace ${package}
+{
+  [Register ("AppDelegate")]
+  public partial class AppDelegate : IOSApplicationDelegate {
+    public override bool FinishedLaunching (UIApplication app, NSDictionary options) {
+      app.SetStatusBarHidden(true, true);
+      var pf = IOSPlatform.register(app, IOSPlatform.SupportedOrients.PORTRAITS);
+      pf.assets().setPathPrefix("assets");
+      PlayN.run(new ${JavaGameClassName}());
+      return true;
+    }
+  }
+
+  public class Application {
+    static void Main (string[] args) {
+      UIApplication.Main (args, null, "AppDelegate");
+    }
+  }
+}
