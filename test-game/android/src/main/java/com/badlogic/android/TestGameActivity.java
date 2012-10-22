b8ed4defee56afca1067defbf0ef44314ce9b6ed
Mario Zechner <contact@badlogicgames.com>
2012-10-22 23:50:53 +0200

    added test project...

---
+++ test-game/android/src/main/java/com/badlogic/android/TestGameActivity.java
@@ -0,0 +1,11 @@
+package com.badlogic.android;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class TestGameActivity extends Activity {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+	}
+}
