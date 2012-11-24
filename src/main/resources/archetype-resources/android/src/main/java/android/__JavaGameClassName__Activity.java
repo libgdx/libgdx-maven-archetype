d87196d5605b85cdfb35ad1cdded48b93f0a0abf
Mario Zechner <contact@badlogicgames.com>
2012-11-24 19:20:26 +0100

    fixed android thingy

--- src/main/resources/archetype-resources/android/src/main/java/android/__JavaGameClassName__Activity.java
+++ src/main/resources/archetype-resources/android/src/main/java/android/__JavaGameClassName__Activity.java
@@ -9,7 +9,6 @@ import android.os.Bundle;
 
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
-import com.tinydungeons.TinyDungeons;
 
 public class ${JavaGameClassName}Activity extends AndroidApplication {
 	
