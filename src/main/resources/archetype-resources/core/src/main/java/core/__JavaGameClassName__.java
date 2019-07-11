296f2b8df6e8c7d4adf87e1ca6be31a59515b062
Piotr Rojewski <rojek_abc@o2.pl>
2019-07-11 14:26:35 +0200

    Update code and HTML to be closer to newest version.

--- src/main/resources/archetype-resources/core/src/main/java/core/__JavaGameClassName__.java
+++ src/main/resources/archetype-resources/core/src/main/java/core/__JavaGameClassName__.java
@@ -3,21 +3,42 @@
 #set( $symbol_escape = '\' )
 package ${package}.core;
 
-import com.badlogic.gdx.graphics.GL30;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class ${JavaGameClassName} extends ApplicationAdapter {
+	private SpriteBatch batch;
+	private Texture img;
+	private BitmapFont font;
 
 	@Override
 	public void create () {
-        Gdx.gl.glClearColor(0, 0, 0, 0);
+		batch = new SpriteBatch();
+		img = new Texture("badlogic.jpg");
+		font = new BitmapFont();
+
+        Gdx.gl.glClearColor(1, 0, 0, 1);
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClear(GL30.GL_COLOR_BUFFER_BIT);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		batch.begin();
+		batch.draw(img, 100, 100);
+        font.draw(batch, "Welcome to libGDX!", 100, 400);
+		batch.end();
 	}
+
+    @Override
+    public void dispose () {
+        batch.dispose();
+        img.dispose();
+        font.dispose();
+    }
+
 }
